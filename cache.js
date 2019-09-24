525ae661484f39ee5373bda6f5830fe20b8d6740
developer <>
2019-09-24 13:54:16 -0700

    Stylistic changes.

--- cache.js
+++ cache.js
@@ -12,33 +12,32 @@ function getHash(data) {
 const writers = new Map();
 
 async function writeJSON(filename, inputData, makeContents) {
-  const fullname = `${outputPath}/${filename}`;
   let writer = writers.get(filename);
-  if (!writer) {
-    writer = (async () => {
-      let contents;
-      try {
-        contents = await fsPromises.readFile(fullname, {encoding: 'utf8'});
-        contents = JSON.parse(contents);
-        if (JSON.stringify(contents.i) === JSON.stringify(inputData)) {
-          return contents.o;
-        }
-      } catch(err) {
-        if (err.code !== 'ENOENT') console.error(err);
+  if (writer) return writer;
+  writer = (async () => {
+    const fullname = `${outputPath}/${filename}`;
+    let contents;
+    try {
+      contents = await fsPromises.readFile(fullname, {encoding: 'utf8'});
+      contents = JSON.parse(contents);
+      if (JSON.stringify(contents.i) === JSON.stringify(inputData)) {
+        return contents.o;
       }
-      contents = await makeContents();
-      // dispatch write job without awaiting
-      (async() => {
-        const contents2 = JSON.stringify({i: inputData, o: contents});
-        await fsPromises.mkdir(outputPath, {recursive: true});
-        fsPromises.writeFile(fullname, contents2);
-      })().finally(() => {
-        writers.delete(filename);
-      });
-      return contents;
-    })();
-    writers.set(filename, writer);
-  }
+    } catch(err) {
+      if (err.code !== 'ENOENT') console.error(err);
+    }
+    contents = await makeContents();
+    // dispatch write job without awaiting
+    (async() => {
+      const contents2 = JSON.stringify({i: inputData, o: contents});
+      await fsPromises.mkdir(outputPath, {recursive: true});
+      fsPromises.writeFile(fullname, contents2);
+    })().finally(() => {
+      writers.delete(filename);
+    });
+    return contents;
+  })();
+  writers.set(filename, writer);
   return writer;
 }
 
