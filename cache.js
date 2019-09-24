39c98f7357aea4af3ee6140fa46e74b38fec4262
developer <>
2019-09-24 02:52:17 -0700

    Greatly simplify cache.js now that it's only doing one thing.

--- cache.js
+++ cache.js
@@ -9,87 +9,34 @@ function getHash(data) {
   return hash.digest('hex');
 }
 
-function path(filename) {
-  return `${outputPath}/${filename}`;
-}
-
-async function mkdir() {
-  return fsPromises.mkdir(outputPath, {recursive: true});
-}
-
-function evaluate(x) {
-  return (typeof x === 'function' ? x() : x);
-}
-
-const readers = new Map();
 const writers = new Map();
 
-async function read(filename, options, decodeContents, makeContents) {
-  let reader = (readers.get(filename) || writers.get(filename));
-  if (!reader) {
-    reader = (async () => {
+async function writeJSON(filename, makeContents) {
+  const fullname = `${outputPath}/${filename}`;
+  let writer = writers.get(filename);
+  if (!writer) {
+    writer = (async () => {
       let contents;
       try {
-        contents = await fsPromises.readFile(path(filename), options);
-        if (decodeContents) contents = decodeContents(contents);
+        contents = await fsPromises.readFile(fullname, {encoding: 'utf8'});
+        contents = JSON.parse(contents);
       } catch(err) {
-        if (makeContents) {
-          if (err.code !== 'ENOENT') console.error(err);
-          contents = await evaluate(makeContents);
-        } else {
-          throw err;
-        }
+        if (err.code !== 'ENOENT') console.error(err);
+        contents = await makeContents();
+        // dispatch write job without awaiting
+        (async() => {
+          const contents2 = JSON.stringify(contents);
+          await fsPromises.mkdir(outputPath, {recursive: true});
+          fsPromises.writeFile(fullname, contents2);
+        })().finally(() => {
+          writers.delete(filename);
+        });
       }
       return contents;
     })();
-    readers.set(filename, reader);
-  }
-  return reader;
-}
-
-async function write(filename, makeContents, encodeContents, mode='wx') {
-  const fullname = path(filename);
-  let reader = readers.get(filename);
-  if (reader) await reader.catch(() => {});
-  let writer = writers.get(filename);
-  if (!writer) {
-    let file;
-    writer = (async () => {
-      await mkdir();
-      try {
-        file = await fsPromises.open(fullname, mode);
-      } catch(err) {
-        if (mode !== 'wx' || err.code !== 'EEXIST') throw err;
-        return;
-      }
-      const contents = await evaluate(makeContents);
-      const contents2 = encodeContents ? encodeContents(contents) : contents;
-      await fsPromises.writeFile(file, contents2);
-      return contents;
-    })().finally(() => {
-      if (file) return file.close();
-    }).catch(async (err) => {
-      await fsPromises.unlink(fullname).catch(() => {});
-      throw err;
-    }).finally(() => {
-      writers.delete(filename);
-    });
     writers.set(filename, writer);
   }
   return writer;
-}
-
-async function writeJSON(filename, makeContents) {
-  let miss = false;
-  let contents = await read(filename, {encoding: 'utf8'}, JSON.parse, () => {
-    miss = true;
-    return evaluate(makeContents);
-  });
-  if (miss) {
-    // dispatch write job without awaiting
-    write(filename, contents, JSON.stringify, 'w').catch(console.error);
-  }
-  return contents;
 }
 
 module.exports = {getHash, writeJSON};
