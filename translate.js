6fba7d6d1a4a4609fe0b03140eb26a9940cf324c
developer <>
2019-09-25 14:54:55 -0700

    Implement source language option in translation.

--- translate.js
+++ translate.js
@@ -7,16 +7,17 @@ const client = new Translate({
 
 async function translate({keys, annotations, srcLang, destLang}) {
   const text = annotations.map(x => x.text);
+  const from = (srcLang === 'auto') ? undefined : srcLang;
   const translations = await cache.writeJSON(
     keys,
     `t.${srcLang}.${destLang}.json`,
     text,
-    () => client.translate(text, destLang)
+    () => client.translate(text, {from, to: destLang})
   );
   translations[1].data.translations.forEach((result, i) => {
     if (annotations[i]) {
       annotations[i].translation = result.translatedText;
-      annotations[i].srcLang = result.detectedSourceLanguage;
+      annotations[i].srcLang = (srcLang === 'auto') ? result.detectedSourceLanguage : srcLang;
       annotations[i].destLang = destLang;
     }
   });
