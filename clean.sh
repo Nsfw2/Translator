897e80971f106418589feb55d1cc130732ed16b0
developer <>
2020-12-22 18:53:36 +0000

    More aggressive cleaning.

--- clean.sh
+++ clean.sh
@@ -3,7 +3,7 @@ grep '}$' log/results \
   | tac \
   | jq -r '.hash' \
   | grep -xP '[0-9a-f]{64}' \
-  | awk '{if (NR > 200000 && !n[$1]) print $1; n[$1]=1}' \
+  | awk '{if (NR > 100000 && !n[$1]) print $1; n[$1]=1}' \
   | while read -r f; do
       find cache/ -name "$f.*.json" -type f -maxdepth 1 -print0 \
         | xargs -0 -r rm --
