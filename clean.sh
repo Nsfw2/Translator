14db7826860765641b7be2ea068570f46a3de5d1
developer <>
2021-01-29 05:20:19 +0000

    Try to make clean.sh more efficient.

--- clean.sh
+++ clean.sh
@@ -5,6 +5,6 @@ grep '^{[^{}]*}$' log/results \
   | grep -xP '[0-9a-f]{64}' \
   | awk '{if (NR > 100000 && !n[$1]) print $1; n[$1]=1}' \
   | while read -r f; do
-      find cache/ -name "$f.*.json" -type f -maxdepth 1 -print0 \
-        | xargs -0 -r rm --
+      compgen -G "cache/$f.*.json" \
+        | xargs -r rm --
     done
